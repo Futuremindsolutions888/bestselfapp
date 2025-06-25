c4a52988cc06a8d6d0c0301372a1fbed39c8e861
Futuremindsolutions888 <futureminingsolutions@gmail.com>
2025-06-26 03:58:03 +0800

    Delete best-self-app directory

--- best-self-app/pages/index.js
+++
@@ -1,8 +0,0 @@
-
-import dynamic from 'next/dynamic';
-
-const BestSelfApp = dynamic(() => import('../src/BestSelfApp'), { ssr: false });
-
-export default function Home() {
-  return <BestSelfApp />;
-}
